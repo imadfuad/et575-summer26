29bdb7173dee39f43b19417a6564cc7465655b3b
imadfuad <imaduddin778@gmail.com>
2026-06-19 06:16:39 +0000

    complete lab 18

--- lab18/lab18_fuad.cpp
+++ lab18/lab18_fuad.cpp
@@ -6,17 +6,43 @@ lab 18, console failure and file streaming
 
 #include<iostream>
 #include "lab18_function_fuad.cpp"
+#include<fstream>
+#include<string>
+
 
 using namespace std;
 
 int main(){
 
-    
     cout << "\n---- example 1: console failure test" << endl;
     checknumber();
+
+    cout << "\n---- example 2: console failure test" << endl;
     float n = validatenumber();
     cout << n << endl;
 
+    cout << "\n---- example 3: read file" << endl;
+    iofiles();
+
+     cout << "\n---- example 4: write file" << endl;
+     writefile();
+
+     cout << "\n---- example 5: append data into a file" << endl;
+     string filename = "outputfile.txt";
+     appendfile(filename);
+
+     cout << "\n---- lab exercise 1: operation function" << endl;
+float result = operation(10, 5);
+cout << "Result = " << result << endl;
+
+cout << "\n---- lab exercise 2a: create and write file" << endl;
+createuserfile();
+
+cout << "\n---- lab exercise 2b: append file" << endl;
+appenduserfile("This line was appended to the file.");
+
+cout << "\n---- lab exercise 2c: overwrite file" << endl;
+overwritefile("newfile.txt", "This is a new overwritten file.");
+
     return 0;
 }
-    
--- lab18/lab18_function_fuad.cpp
+++ lab18/lab18_function_fuad.cpp
@@ -46,3 +46,145 @@ float validatenumber(){
 
     }while(true);
 }
+
+// example 3: input and output files
+#include<fstream>
+#include<string>
+
+void iofiles(){
+    // declare the objects to handle files
+    // input file object 'fin'
+    ifstream fin;
+
+    // output file object 'fout'
+    ofstream fout;
+
+    // read file 'samplefile.txt'
+    // use the 'fin' object along with the 'open' method to open the txt file
+    fin.open("samplefile.txt");
+
+    // open output file
+    fout.open("outputfile.txt");
+
+    // declare a variable 'line' to temporary store each line of 'samplefile.txt'
+    string line;
+
+       /*
+    while loop to go through each line in the txt file.
+    for each line, we are going to use the getline() method to read and hold each line
+    of the txt file in variable 'line'
+    */
+    int linecounter = 1;
+
+    while(getline(fin, line)){
+        cout << "Line " << linecounter << " = \t" << line << endl;
+        fout << "Line " << linecounter << " = \t" << line << endl;
+        linecounter++;
+    }
+
+    // close the file
+    fin.close();
+    fout.close();
+
+    
+}
+
+// example 4: write file
+// writing a file with an 'open' method, c++ will automatically create the file if it doesn't exist
+// if the file exists, then c++ will overwrite file
+void writefile(){
+    // output file object 'fout'
+    ofstream fout;
+
+    fout.open("outputfile.txt");
+
+    // to write in a file, we use the output file object fout
+    fout << "Good Morning!" << endl;
+
+    for(int n = 3; n > 0; n--){
+        fout << n << endl;
+    }
+
+    fout << "End of file" << endl;
+
+    fout.close();
+
+    
+}
+
+
+// example 5: appending data to an existing file
+// fout.open() we need to add the argument ios::app
+void appendfile(string filename){
+    // output file object 'fout'
+    ofstream fout;
+
+    fout.open(filename, ios::app);
+
+    fout << "Md Imad Fuad" << endl;
+
+    fout.close();
+}
+
+// lab exercise 1: operation function
+float operation(int num1, int num2){
+    char symbol;
+    float result;
+
+    cout << "Enter an operation symbol (+, -, *, /, %): ";
+    cin >> symbol;
+
+    while(cin.fail() || (symbol != '+' && symbol != '-' && symbol != '*' && symbol != '/' && symbol != '%')){
+        cin.clear();
+        cin.ignore(10000, '\n');
+
+        cout << "Invalid symbol. Enter +, -, *, /, or %: ";
+        cin >> symbol;
+    }
+
+    if(symbol == '+')
+        result = num1 + num2;
+    else if(symbol == '-')
+        result = num1 - num2;
+    else if(symbol == '*')
+        result = num1 * num2;
+    else if(symbol == '/')
+        result = float(num1) / num2;
+    else
+        result = num1 % num2;
+
+    return result;
+}
+
+// lab exercise 2a: create and write a new text file
+void createuserfile(){
+    ofstream fout;
+
+    fout.open("data_user.txt");
+
+    fout << "This is my output file - Md Imad Fuad.\n";
+
+    fout.close();
+}
+
+// lab exercise 2b: append text to existing text file
+void appenduserfile(string message){
+    ofstream fout;
+
+    fout.open("data_user.txt", ios::app);
+
+    fout << message << endl;
+
+    fout.close();
+}
+
+// lab exercise 2c: create a new file and overwrite text
+void overwritefile(string filename, string text){
+    ofstream fout;
+
+    fout.open(filename);
+
+    fout << text << endl;
+
+    fout.close();
+}
